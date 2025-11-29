fca867e6daf91eac550b8165f568e8d078b23adf
Bhavya Gautam <gautambhavya899@gmail.com>
2025-11-29 07:57:38 +0000

    Solved leetcode 88, merge sorted array

--- 88_merge_sorted_array/solution.c
+++ 88_merge_sorted_array/solution.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void merge_sorted_array(int *arr1, int n, int *arr2, int m){
-    
+void merge_sorted_array(int *arr1, int m, int *arr2, int n){
+    int i,j,k;
+    i=m-1;j=n-1;k=m+n-1;
+    while(i>=0 && j>=0) {
+    	if(*(arr1+i) > *(arr2+j)) {
+	    *(arr1+k--) = *(arr1+i--);
+	} else {
+	    *(arr1+k--) = *(arr2+j--);
+	}
+    }
+    while(i>=0) {
+    	*(arr1+k--) = *(arr1+i--);
+    }
+    while(j>=0){
+    	*(arr1+k--) = *(arr2+j--);
+    }
 }
 int main(){
     int arr1[] = {1,2,3,0,0,0};
     int arr2[] = {2,5,6};
-    merge_sorted_array(arr1,6,arr2,3);
+    merge_sorted_array(arr1,3,arr2,3);
+
+    for(int i=0;i<6;i++){
+    	printf("%d ", *(arr1 + i));
+    }
+    return 0;
 }
--- 88_merge_sorted_array/solution.cpp
+++ 88_merge_sorted_array/solution.cpp
@@ -1,16 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 class Solution {
 public:
-    void merge_sorted_array(vector<int> &v1, int n, vector<int> &v2, int m){
-
+    void merge_sorted_array(vector<int> &nums1, int m, vector<int> &nums2, int n){
+	// In order to merge two sorted array we know both the arrays are sorted in non-decreasing order the largest element will be at the end of each array. So we start comparing from the last of each array. This way we would not have to make space of each new element like we would have to do if we started from the beginning.
+        int i,j,k;
+        i = m-1;
+        j = n-1;
+        k = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while(i>=0){
+            nums1[k--] = nums1[i--];
+        }
+        while(j>=0){
+            nums1[k--] = nums2[j--];
+        }
     }
-}
+};
 
-int main(){
+int main() {
     vector<int> v1 = {1,2,3,0,0,0};
     vector<int> v2 = {2,5,6};
     Solution sol;
-    sol.merge_sorted_array(v1,6,v2,3);
-    
+    sol.merge_sorted_array(v1,3,v2,3);
+    for(int i=0;i<v1.size();i++){
+    	cout<< v1[i] << " ";
+    }
+    return 0;
 }
